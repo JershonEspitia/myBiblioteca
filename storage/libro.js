6e4234a5555c82802b9024d7d3e7851b4af33b75
JershonEspitia <jershonespitiarey@gmail.com>
2023-10-02 19:32:24 -0500

    feat: :sparkles: Finished libro.js
    Se finaliza las consultas de libro.js y sus validaciones en el archivo validations.js

--- storage/libro.js
+++ storage/libro.js
@@ -1,52 +1,60 @@
 import { env } from "../config.js";
+import { validationLibro } from "../storage/validations.js";
 
-const uri = `${env.ssl + env.hostName}:${env.port}/`;
+const uri = `${env.ssl + env.hostName}:${env.port}`;
+const endPoint = "/libro/"
 const config = {
   method: undefined,
   headers: { "Content-Type": "application/json" },
+  body: undefined
 };
 
 export const getAll = async () => {
   config.method = "GET";
-  // config.body = JSON.stringify(obj);
-  let res = await (await fetch(`${uri}libro`, config)).json();
+  let res = await (await fetch(`${uri}${endPoint}`, config)).json();
   return res;
 };
 
 export const postAll = async (obj) => {
-  config.method = "POST";
-  config.body = JSON.stringify(obj);
-  let res = await (await fetch(`${uri}libro`, config)).json();
-  return res;
+
+  const value = validationLibro(obj, "POST");
+  console.log("value",value);
+
+  if (value == true) {
+    config.method = "POST";
+    config.body = JSON.stringify(obj);
+    let res = await (await fetch(`${uri}${endPoint}`, config)).json();
+    return res;
+  } else {
+    return "No se pudo realizar el POST";
+  }
 };
 
 export const deleteOne = async (id) => {
   if (typeof id !== "number")
     return { status: 400, message: `El dato ${id} no cumple con el formato` };
   config.method = "DELETE";
-  // config.body = JSON.stringify(obj);
-  let res = await (await fetch(`${uri}libro/${id}`, config)).json();
+  let res = await (await fetch(`${uri}${endPoint}${id}`, config)).json();
   return res;
 };
 
-export const putOne = async (obj={}) => {
+export const putOne = async (obj = {id: -999, autorId: -999, categoriaId: -999, editorialId: -999, titulo: -999, fechaLanzamiento: -999, isbn: -999, numPaginacion: -999, estadoId: -999}) => {
 
-  if(!obj.id) return { status: 400, message: `Debe enviar los datos`};
-
-  const {id, autorId, categoriaId, editorialId, titulo, fechaLanzamiento, isbn, numPaginacion, estadoId} = obj;
-  let date = new Date(fechaLanzamiento);
-  if (!(date && date.getFullYear() <= 2040)) return { status: 400, message: `El dato ${id} no cumple con el formato`};
-  if (typeof id !== "number") return { status: 400, message: `El dato ${id} no cumple con el formato`};
-  if (typeof autorId !== "number") return { status: 400, message: `El dato ${id} no cumple con el formato`};
-  if (typeof categoriaId !== "number") return { status: 400, message: `El dato ${id} no cumple con el formato`};
-  if (typeof editorialId !== "number") return { status: 400, message: `El dato ${id} no cumple con el formato`};
-  if (typeof titulo !== "string") return { status: 400, message: `El dato ${titulo} no cumple con el formato`};
-  if (typeof isbn !== "string") return { status: 400, message: `El dato ${isbn} no cumple con el formato`};
-  if (typeof numPaginacion !== "number") return { status: 400, message: `El dato ${numPaginacion} no cumple con el formato`};
-  if (typeof estadoId !== "number") return { status: 400, message: `El dato ${estadoId} no cumple con el formato`};
-
-  config.method = "PUT";
-  config.body = JSON.stringify(obj);
-  let res = await (await fetch(`${uri}libro/${id}`, config)).json();
-  return res;
+  const value = validationLibro(obj);
+  console.log("value",value);
+  
+  if (value === true) {
+    config.method = "PUT";
+    config.body = JSON.stringify(obj);
+    let res = await (await fetch(`${uri}${endPoint}${obj.id}`, config)).json();
+    return res;
+  } else {
+    return "No se pudo realizar el PUT";
+  }
 };
+
+// LLAMADO A LOS METODOS
+// console.log(await getAll());
+// console.log(await postAll({autorId: 1, categoriaId: 1, editorialId: 1, titulo: "Mi libro", fechaLanzamiento: "2023-04-02", isbn: "isbn1", numPaginacion: 120, estadoId: 1}));
+// console.log(await deleteOne(1));
+// console.log(await putOne({id: 1, autorId: 11, categoriaId: 11, editorialId: 1, titulo: "Mi libro 3", fechaLanzamiento: "2023-04-02", isbn: "isbn1", numPaginacion: 120, estadoId: 1}));
---
+++ storage/validations.js
@@ -0,0 +1,38 @@
+export const validationLibro = (obj, method)=>{
+
+    const {id, autorId, categoriaId, editorialId, titulo, fechaLanzamiento, isbn, numPaginacion, estadoId} = obj;
+    
+    let date = new Date(fechaLanzamiento);
+  
+    if (!fechaLanzamiento) return { status: 400, message: `Por favor ingrese una fecha.`}
+    if (!(date && date.getFullYear() <= 2040)) return { status: 400, message: `El dato (fechaLanzamiento) ${fechaLanzamiento} no cumple con el formato`};
+  
+    if (method !== "POST") {
+        if (!id) return { status: 400, message: `Por favor ingrese el id.`};
+        if (typeof id !== "number") return { status: 400, message: `El dato (id) ${id} no cumple con el formato`};
+    }
+        
+    if (!autorId) return { status: 400, message: `Por favor ingrese el id del autor.`}
+    if (typeof autorId !== "number") return { status: 400, message: `El dato (autorId) ${autorId} no cumple con el formato`};
+    
+    if (!categoriaId) return { status: 400, message: `Por favor ingrese el id de la categoria.`}
+    if (typeof categoriaId !== "number") return { status: 400, message: `El dato (categoriaId) ${categoriaId} no cumple con el formato`};
+  
+    if (!editorialId) return { status: 400, message: `Por favor ingrese el id de la editorial.`}
+    if (typeof editorialId !== "number") return { status: 400, message: `El dato (editorialId) ${editorialId} no cumple con el formato`};
+  
+    if (!titulo) return { status: 400, message: `Por favor ingrese un titulo.`}
+    if (typeof titulo !== "string") return { status: 400, message: `El dato (titulo) ${titulo} no cumple con el formato`};
+  
+    if (!isbn) return { status: 400, message: `Por favor ingrese el isbn.`}
+    if (typeof isbn !== "string") return { status: 400, message: `El dato (isbn) ${isbn} no cumple con el formato`};
+  
+    if (!numPaginacion) return { status: 400, message: `Por favor ingrese el numero de paginacion.`}
+    if (typeof numPaginacion !== "number") return { status: 400, message: `El dato (numPaginacion) ${numPaginacion} no cumple con el formato`};
+  
+    if (!estadoId) return { status: 400, message: `Por favor ingrese el estado del id.`}
+    if (typeof estadoId !== "number") return { status: 400, message: `El dato (estadoId) ${estadoId} no cumple con el formato`};
+
+    return true;
+};
+
